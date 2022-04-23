43048734279a3210d958ee0762e421dba2585a30
Omar <ofernandomm@gmail.com>
2022-04-22 19:51:15 -0500

    Se agrego el codigo en app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.use(express.static('./public'))
+
+app.listen(8000, ()=> {
+    console.log('Servidor corriendo en puerto 8000')
+})
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.resolve(__dirname,'./views/index.html'))
+})
