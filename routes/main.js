5b2af4105a10f2d085e4a2ed6b18bc5e583c2482
ivanperciante <ivanperciante21@outlook.com.ar>
2022-05-14 17:23:51 -0300

    Cambios en formularios

--- routes/main.js
+++ routes/main.js
@@ -5,7 +5,7 @@ const mainController = require("../controllers/mainController");
 router.get("/", mainController.index);
 router.get("/productDetail", mainController.productDetail);
 router.get("/cart", mainController.cart);
-router.get("/singUp", mainController.signUp);
+router.get("/signUp", mainController.signUp);
 router.get("/logIn", mainController.logIn);
 
 module.exports = router
