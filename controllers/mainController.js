dad0a252633700d55ca23e85e7754c94e7dc6dc9
Ignacio Alquati <ignacioalquati@MacBook-Pro.fibertel.com.ar>
2022-05-24 18:25:47 -0300

    Agrego dinamismo a index, detalles en css y ejs de index, productDetail, header

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,6 +1,13 @@
+const fs = require('fs');
+const path = require ('path')
+
+
+const productsPath = path.join(__dirname, '../data/products.json');
+const products = JSON.parse(fs.readFileSync(productsPath, 'utf-8'))
+
 const mainController = {
     index: (req, res) => {
-        res.render("index")
+        res.render("index", {products:products})
     },
 
     productDetail: (req, res) => {
