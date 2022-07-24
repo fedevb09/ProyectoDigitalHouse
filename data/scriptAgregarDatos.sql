f0f8c3d5c8f222462bf0c687df9a4fa417a17b28
Omar <ofernandomm@gmail.com>
2022-07-24 11:17:23 -0500

    agregando script para completar base de datos de productos

--- data/scriptAgregarDatos.sql
+++ data/scriptAgregarDatos.sql
@@ -1,2 +1,4 @@
 INSERT INTO artplace_db1.products (productName,categoryId, description, price, img1, img2, img3, sizes, creatorName, stock)
-VALUES ('Cuadro de boba fett',1, 'un cuadro muy bonito', '300', 'Cuadro-bobba.png', 'Cuadro-boba-render-2.png', 'Cuadro-boba-render-1.png', '20', 'JJ. Abrams', '10');
+VALUES 
+        ('Cuadro de boba fett',1, 'un cuadro muy bonito', '300', 'Cuadro-bobba.png', 'Cuadro-boba-render-2.png', 'Cuadro-boba-render-1.png', '20', 'JJ. Abrams', '10'),
+        ('Cerati',1, 'Cerati', '300', 'cerati2.jpg', 'cerati2.jpg', 'cerati2.jpg', '10', 'Ceratiart', '5');
