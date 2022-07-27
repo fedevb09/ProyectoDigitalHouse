026551bf4b61fb49aae6b801b4640aff5690a78f
nachoalquati <nachoalquati@gmail.com>
2022-07-26 23:47:12 -0300

    js

---
+++ public/js/darkMode.js
@@ -0,0 +1,16 @@
+window.addEventListener('load', function(){
+
+    let body = document.querySelector('body');
+    let card = document.querySelector('.product');
+    let activeButton = document.querySelector('#activeDarkMode')
+
+
+    activeButton.addEventListener('click', function(e){
+
+        body.toggleAttribute = ;
+
+    })
+
+
+
+})
