f8b3c4ae403cd83fa5f90e434670494989f2e8fd
nachoalquati <nachoalquati@gmail.com>
2022-08-05 00:24:01 -0300

    Fix front

--- public/js/search.js
+++ public/js/search.js
@@ -3,6 +3,7 @@ window.onload = function () {
     let searchInput = document.querySelector('.header-searchbar-input')
     let resultsDiv = document.querySelector('#search-bar')
     let main = document.querySelector('main')
+    let lis = document.querySelector('#lis')
     
     
     main.addEventListener('click', function(){
@@ -29,29 +30,36 @@ window.onload = function () {
                     if (lowerCaseProduct.indexOf(inputText) !== -1) {
                         resultsDiv.innerHTML += `
     
-                    <li id='lis'><a href='http://localhost:8000/products/${product.id}'>${product.productName}<p>${product.creatorName}</p></a></li>
+                    <li id='lis'><a id='slinks' href='http://localhost:8000/products/${product.id}'>${product.productName}<p>${product.creatorName}</p></a></li>
     
                     `
-
-                    }
-                   
-                   
+                    
+                }
+                
+                
                   
 
                 });
-
+                
                 if(resultsDiv.innerHTML === ''){
                     resultsDiv.innerHTML += `
-    
+                    
                     <li>No encontramos ese producto...</li>
-    
+                    
                     `
                 }
-
-            }
                 
-                searchInput.addEventListener('keyup', search)
-
-
+            }
+            
+            searchInput.addEventListener('keyup', search)
+            resultsDiv.addEventListener('focus', function(){
+                searchInput.innerHTML += 'asd'
+                console.log('asfrasf');
+            })
+            
+            lis.addEventListener('click', function(){
+                console.log('a');
+            })
+            
         })
 }
