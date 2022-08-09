49c7515927b508c7bb55d1aa4e35ab8b729057c2
Omar <ofernandomm@gmail.com>
2022-08-09 09:57:14 -0500

    fix validaciones en creación de producto

--- public/js/creacionProductoValidacion.js
+++ public/js/creacionProductoValidacion.js
@@ -1,6 +1,5 @@
 window.onload = function(){
     
-
     let form = document.querySelector('.form');
 
     form.addEventListener('submit',(e)=>{
@@ -8,14 +7,28 @@ window.onload = function(){
         let errors=[]
 
         let name = form.productName
+        let description = form.description
+        let price = form.price
 
-        if(name.value==""){
-            errors.push('campo obligatorio')
-            document.querySelector('#message-error').innerText = 'campo obligatorio';
+        if(name.value=="" || name.value.length<5 ){
+            errors.push('campo obligatorio con minimo 5 caracteres')
+            document.querySelector('#message-error').innerText = 'campo obligatorio con mínimo 5 caracteres';
+        
+        }else if(isNaN(price.value)==true){
+            errors.push('Debe ser un número')
+            document.querySelector('#message-error3').innerText = 'Debe ser un número';
+
+        }else if(description.value.length<20){
+            errors.push('min 20 caracteres')
+            document.querySelector('#message-error2').innerText = 'min 20 caracteres';
         }
-
+        
         if(errors.length>0){
             e.preventDefault()
+            swal({
+                title: "Hemos encontrado un error en el formulario",
+                text: "Algunos campos no cumplen los requerimientos",
+              });
         }else{
             form.submit()
         }
