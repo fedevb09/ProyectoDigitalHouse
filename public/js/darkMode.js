c81f7e847e4b551351d9ccaedef3bce6b091dec0
nachoalquati <nachoalquati@gmail.com>
2022-08-10 00:10:17 -0300

    Dark mode

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -1,14 +1,26 @@
 window.addEventListener('load', function(){
 
     let body = document.querySelector('body');
-    let card = document.querySelector('.product');
+    let cards = document.querySelectorAll('.product');
     let activeButton = document.querySelector('#activeDarkMode')
+    let titles = document.querySelectorAll('.featured')
+    let innerBox = document.querySelector('.inner-border')
+    let icon = document.querySelector('#activeDarkMode i')
 
 
     activeButton.addEventListener('click', function(e){
 
-       // body.toggleAttribute = ;
-
+        body.classList.toggle('darkBody')
+        cards.forEach(card => {
+            card.classList.toggle('darkCard')
+        });
+        titles.forEach(title => {
+            title.classList.toggle('darkTitles')
+        });
+        activeButton.classList.toggle('dark-ligth-mode')
+        activeButton.classList.toggle('ligth-mode')
+        icon.classList.toggle('fa-moon')
+        icon.classList.toggle('fa-sun')
     })
 
 
