2b828dc8d4ba5f43bf4d42ec13986dbcd5eeb1c9
Omar <ofernandomm@gmail.com>
2022-08-19 13:31:09 -0500

    avance en carrito

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,6 +1,38 @@
 window.addEventListener('load', function(){
+    let cart=[]
+    let btn=document.querySelector('.buy-button');
+    let id=document.querySelector('.productId').innerHTML;
+    let carrito=JSON.parse(localStorage.getItem('carrito'))
+    console.log(carrito)
+    if(carrito == null){
+        console.log('el carrito está vacio')
+    }else{
+        console.log('el carrito tiene productos')
+    }
 
+    btn.addEventListener('click', function() {
 
+        if(carrito != null){
+            let nuevoCarrito = cart.push(carrito)
+            console.log(cart)
+            console.log(typeof(cart))
+            nuevoCarrito.push(id)
+            console.log(nuevoCarrito)
+            localStorage.setItem('carrito',JSON.stringify(nuevoCarrito))
+            console.log(nuevoCarrito)
+            console.log('se guardo en carrito viejo')
 
-
+        }else{
+            console.log(id)
+            cart.push(id)
+            console.log(cart)
+            console.log(typeof(cart))
+            localStorage.setItem('carrito', JSON.stringify(cart))
+            console.log('se guardo en carrito nuevo')
+            let nuevoCarro=JSON.parse(localStorage.getItem('carrito', carrito))
+            console.log(nuevoCarro)
+            console.log(typeof(nuevoCarro))
+            
+        }
+    });
 })
