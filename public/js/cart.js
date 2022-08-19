d7cb171494566460801a6691c5754a95ca00ae0c
Omar <ofernandomm@gmail.com>
2022-08-19 14:42:02 -0500

    fix lógica del carrito en el front

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,10 +1,14 @@
 window.addEventListener('load', function(){
-    let cart=[]
     let btn=document.querySelector('.buy-button');
     let id=document.querySelector('.productId').innerHTML;
     let carrito=JSON.parse(localStorage.getItem('carrito'))
-    console.log(carrito)
+
     if(carrito == null){
+        localStorage.setItem('carrito','[]')
+        console.log('nuevo carrito activado')
+    }
+
+    if(carrito.length==0){
         console.log('el carrito está vacio')
     }else{
         console.log('el carrito tiene productos')
@@ -13,26 +17,15 @@ window.addEventListener('load', function(){
     btn.addEventListener('click', function() {
 
         if(carrito != null){
-            let nuevoCarrito = cart.push(carrito)
-            console.log(cart)
-            console.log(typeof(cart))
+            let nuevoCarrito = JSON.parse(localStorage.getItem('carrito'))
             nuevoCarrito.push(id)
-            console.log(nuevoCarrito)
-            localStorage.setItem('carrito',JSON.stringify(nuevoCarrito))
-            console.log(nuevoCarrito)
+            localStorage.setItem('carrito', JSON.stringify(nuevoCarrito))
             console.log('se guardo en carrito viejo')
 
         }else{
-            console.log(id)
-            cart.push(id)
-            console.log(cart)
-            console.log(typeof(cart))
-            localStorage.setItem('carrito', JSON.stringify(cart))
+            carrito.push(id)
+            localStorage.setItem('carrito', JSON.stringify(carrito))
             console.log('se guardo en carrito nuevo')
-            let nuevoCarro=JSON.parse(localStorage.getItem('carrito', carrito))
-            console.log(nuevoCarro)
-            console.log(typeof(nuevoCarro))
-            
         }
     });
 })
