4726d071c6abc485a1dae57676b280bce053aadd
Omar <ofernandomm@gmail.com>
2022-08-19 16:50:58 -0500

    metodos para oirganizar data

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,5 +1,7 @@
 window.addEventListener('load', function(){
     let carrito=JSON.parse(localStorage.getItem('carrito'))
+    console.log(typeof(carrito))
+    console.log(carrito)
 
     if(carrito.length==0){
         console.log('el carrito está vacio')
@@ -8,12 +10,27 @@ window.addEventListener('load', function(){
         console.log(carrito)
     }
 
+    //muestra los valores únicos
     const unique = (value, index, self) => {
         return self.indexOf(value) === index
       }
     const uniqueProducts = carrito.filter(unique)
     console.log(uniqueProducts)
 
-    
+
+    //cuenta los valores únicos
+    const countUnique = arr => {
+        const counts = {};
+        for (let i = 0; i < arr.length; i++) {
+           counts[arr[i]] = 1 + (counts[arr[i]] || 0);
+        };
+        return counts;
+     };
+
+     productsCount=countUnique(carrito)
+     console.log(productsCount);
+
+
+
 
 })
