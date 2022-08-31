463e5f718ee6e5f25225ec61630c540ac467e748
nachoalquati <nachoalquati@gmail.com>
2022-08-30 21:17:31 -0300

    Agrego productos a la DB y algunos fix en dark mode

--- public/js/darkMode.js
+++ public/js/darkMode.js
@@ -1,19 +1,74 @@
 window.addEventListener('load', function(){
 
+
+
+    let darkMode =JSON.parse(localStorage.getItem('darkMode'));
+    if(!darkMode){
+
+        localStorage.setItem('darkMode', JSON.stringify(false));
+    }
+    console.log(darkMode);
+
     let body = document.querySelector('body');
     let cards = document.querySelectorAll('.product');
-    let activeButton = document.querySelector('#activeDarkMode')
-    let titles = document.querySelectorAll('.featured')
-    let innerBox = document.querySelector('.inner-border')
-    let icon = document.querySelector('#activeDarkMode i')
+    let activeButton = document.querySelector('#activeDarkMode');
+    let titles = document.querySelectorAll('.featured');
+    let innerBox = document.querySelector('.inner-border');
+    let icon = document.querySelector('#activeDarkMode i');
     let carrusel = document.querySelector('.productCarousel');
+    let profileTexts = document.querySelectorAll('.profile-texts')
+
+
+
+
+
+    if(darkMode){
+
+        body.classList.add('darkBody')
+        body.classList.add('detail-dark-mode')
+        cards.forEach(card => {
+            card.classList.add('darkCard')
+        });
+        titles.forEach(title => {
+            title.classList.add('darkTitles')
+        });
+        
+        activeButton.classList.add('ligth-mode')
+        icon.classList.add('fa-moon')
+        icon.classList.add('fa-sun')
+        if(carrusel){
+            carrusel.classList.add('dark-carrusel')
+        }
+
+        profileTexts.forEach(element => {
+            element.classList.add('dark-profile')
+        });
+
+      
+
+        
+        
+    }
+
+
     
     
     
     activeButton.addEventListener('click', function(e){
-        console.log('asdasd');
+        
+        if(darkMode){
 
-        let profileTexts = document.querySelectorAll('.profile-texts')
+            darkMode = false;
+        }
+        else{
+            darkMode = true;
+        }
+
+        localStorage.setItem('darkMode', JSON.stringify(darkMode));
+
+
+
+        
         
         body.classList.toggle('darkBody')
         cards.forEach(card => {
@@ -33,6 +88,10 @@ window.addEventListener('load', function(){
         profileTexts.forEach(element => {
             element.classList.toggle('dark-profile')
         });
+
+        if(darkMode){
+        
+        }
     })
 
 
